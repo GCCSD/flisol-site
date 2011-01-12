ab57fd9a0aea885f88c96bbbaa52986977a3433f
InFog <junior@casoft.info>
2011-01-12 12:12:49 -0200

    Primeiro commit. Adicionado o site do flisol 2010.

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pr-br">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+        <script type="text/javascript" src="javascript/jquery-1.4.2.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.tools.min.js"></script>
+        <link rel="stylesheet" href="css/site.css" type="text/css" media="screen" />
+        <title>Flisol :: Santos :: 2010</title>
+        <script type="text/javascript">
+            $(document).ready( function() {
+				$('#menu').tabs('#content > div');
+			})
+        </script>
+    </head>
+    <body>
+        <div id="wrapper">
+			<div id="top">
+				<img src="imagens/banner.png" alt="Flisol em Santos, 24 de abril de 2010" />
+			</div><!-- top -->
+            <div id="menu">
+                <ul>
+                    <li><a href="#flisol">Sobre o Flisol</a></li>
+                    <li><a href="#quando">Quando/Onde?</a></li>
+                    <li><a href="#preparar">Prepare-se</a></li>
+                    <li><a href="#programacao">Programação</a></li>
+                    <li><a href="#gccsd">Organizadores</a></li>
+                </ul>
+            </div><!-- menu -->
+            <div id="content">
+                <div id="flisol">
+                    <h1>Flisol</h1>
+                    <p>O <strong>Festival Latino Americano de Instalação de Software Livre</strong> (<strong>Flisol</strong>) é um evento regional, realizado anualmente, e que ocorre de forma simultânea em diversas cidades da América Latina. O Flisol é um evento descentralizado, onde cada local escolhido realiza seu festival simultaneamente com os outros pontos espalhados pela América Latina.</p>
+
+					<p>O evento é livre, e nesse dia há a instalação de <strong>Software Livre</strong>, como distribuições de <strong>Gnu/Linux</strong>, <strong>sistemas BSD</strong>, e aplicativos livres para Windows em geral.</p>
+
+					<h2>Objetivo do evento</h2>
+					<p>O objetivo do Flisol é promover o uso de software livre em geral, além de criar interações entre usuários e desenvolvedores.</p>
+                </div>
+                <div id="quando">
+                    <h1>Quando? Onde?</h1>
+                    <p>O Flisol 2010 acontecerá no dia <strong>24 de Abril</strong>, um sábado. O horário do evento é das <strong>9:00h às 17:00h</strong>.</p>
+                    <p>O Flisol 2010 será realizado na Unimonte, Campus Victorio Lanza, que fica na Rua Bras Cubas, 344, na Vila Mathias, em Santos - SP.</p>
+                    <p><img src="imagens/mapa.png" alt="Mapa Flisol" /></p>
+                </div>
+                <div id="preparar">
+					<h1>Prepare-se para o Flisol Santos 2010!</h1>
+					<p>Aqui está uma lista do que fazer para se preparar para um Flisol:</p>
+					<h2>O que levar</h2>
+					<img src="imagens/laptop.png" alt="Laptop" style="float: left;" /><p>Se você quiser instalar software livre em seu computador ou notebook basta leva-lo ao evento. Lá pessoas preparadas e experiêntes farão a instalação para você.</p>
+					<img src="imagens/cdrom.png" alt="CD/DVD" style="float: right;" /><p>Você também pode levar a mídias (CDs, DVDs, Pendrives, etc) para voltar para casa com diversos softwares livres para várias áreas.</p>
+					<p>É claro que você também pode ir ao evento levando apenas a sua curiosidade e vontade de aprender mais sobre o mundo do Software Livre e de Código Aberto. Durante o Flisol você vai entender o motivo de pessoas, empresas e governos no mundo tudo estarem estudando e migrando para esse tipo de tecnologia.</p>
+                </div>
+                <div id="programacao">
+                    <h1>Programação</h1>
+                    <h2>Install Fest</h2>
+                    <p><strong>Horário</strong>: Acontecerá durante todo o dia.</p>
+                    <br />
+                    <br />
+                    <h2>Palestra: Software Livre</h2>
+                    <p><strong>Palestrante</strong>: <a href="http://kretcheu.com.br">Kretcheu</a>.</p>
+                    <p><strong>Horário</strong>: 12:00h.</p>
+                    <br />
+                    <br />
+                    <h2>13:00h as 13:30h - Almoço</h2>
+                    <br />
+                    <br />
+                    <h2>Palestra: Desenvolvimento com Software Livre</h2>
+                    <p><strong>Palestrante</strong>: <a href="http://infog.casoft.info">InFog</a>.</p>
+                    <p><strong>Horário</strong>: 13:30h.</p>
+                    <br />
+                    <br />
+                    <h2>Palestra: Desktop Livre</h2>
+                    <p><strong>Palestrante</strong>: <a href="http://thiagofazzio.com.br">Guariní</a>.</p>
+                    <p><strong>Horário</strong>: 15:00h.</p>
+                </div>
+                <div id="gccsd">
+                    <h1>O GCC-SD</h1>
+                    <p>O <a href="http://gccsd.com.br" title="Blog do GCC-SD"><strong>Grupo de Compartilhamento do Conhecimento Santos Dumont</strong></a> é um grupo formado principalmente por usuários de software livre e que tem como objetivo a troca de conhecimento e informações entre os membros.</p>
+					<h2>Como o Grupo Surgiu?</h2>
+
+					<p>O Grupo de Compartilhamento do Conhecimento Santos Dumont surgiu em 2007 para a preparação do primeiro Flisol em Santos. Na época o grupo ainda não tinha esse nome e os únicos membros que participaram daquele Flisol e que continuaram participando dos encontros do grupo foram o <a href="http://infog.casoft.info" title="Blog do InFog">InFog</a> e o <a href="http://kretcheu.com.br" title="Kretcheu Vídeo Blog">Kretcheu</a>.</p>
+
+					<p>E foi durante 2007 que o grupo foi batizado de <strong>GCC Santos Dumont</strong>. Antes disso algumas outras opções de nome foram consideradas, já que nós não queriamos seguir o velho padrão GUSLXX (Grupo de Usuários de Software Livre de “Algum Lugar”). Um dos nomes sugeridos foi Marruá, que na cultura nordestina é usado para definir os cavalos selvagens que nunca foram domados, por isso não livres. É um ótimo nome, com um significado muito legal para o grupo, mas a opinião dos membros foi de que le não soava bem.</p>
+
+					<p>Já no ano de 2008 para a realização do segundo Flisol contamos com a participação de mais pessoas, como é o caso do Sprity e do <a href="http://blog.thiagofazzio.com.br" title="Blog do Guariní">Guariní</a>, que conhecemos durante uma palestra de software livre realizada pelo Kretcheu na Unimonte.</p>
+
+					<p>Durante o ano de 2008, o grupo teve a participação de apenas três membros, Guariní, InFog e Kretcheu. Tudo bem que lá de vez em quando aparecia algum rosto novo, mas acho que isso aconteceu uma ou duas vezes.</p>
+
+					<p>No Flisol de 2009 novos membros se juntaram ao grupo, um deles é o Phoenix, e agora o compartilhamento de informações e a disseminação do software livre está ganhando força na região de Santos!</p>
+
+					<h2>Sobre o nome...</h2>
+
+					<p>O nome GCC faz alusão ao famoso <strong>Gnu C Compiler</strong>, o compilador do projeto Gnu, e a homenagem à Santos Dumont foi feita por que ele não patenteava suas invenções, acreditando que o progresso deve acontecer à toda a humanidade e não apenas à algumas pessoas.</p>
+
+                </div>
+            </div><!-- content -->
+            <div id="footer">
+				<em>Flisol 2010</em>
+			</div>
+        </div><!-- wrapper -->
+    </body>
+</html>
