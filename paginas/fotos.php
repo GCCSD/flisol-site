352a56aba0c325ad91a50d73f8ec0ed851d6aab4
InFog <junior@casoft.info>
2011-03-07 16:49:34 -0300

    Ajuste nas fotos.

--- paginas/fotos.php
+++ paginas/fotos.php
@@ -1,6 +1,20 @@
 <h1>Fotos</h1>
-<p>Veja as fotos dos Flisois anteriores.</p>
-<img src="<?php echo URL; ?>imagens/fotos/flisol2010-1.jpg" alt="Flisol 2010" />
-<img src="<?php echo URL; ?>imagens/fotos/flisol2010-2.jpg" alt="Flisol 2010" />
-<img src="<?php echo URL; ?>imagens/fotos/flisol2010-3.jpg" alt="Flisol 2010" />
-<img src="<?php echo URL; ?>imagens/fotos/flisol2010-4.jpg" alt="Flisol 2010" />
+<p>Veja as fotos dos eventos anteriores.</p>
+<table>
+    <tr>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-1.jpg" alt="Flisol 2010" />
+        </td>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-2.jpg" alt="Flisol 2010" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-3.jpg" alt="Flisol 2010" />
+        </td>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-4.jpg" alt="Flisol 2010" />
+        </td>
+    </tr>
+</table>
