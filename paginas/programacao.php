d51a39b3a3f13d7f06c79dcca2d6791e55feaf05
Diego Souza <diegosouza.br@gmail.com>
2011-03-07 12:31:10 -0300

    renomeando arquivos html para php

---
+++ paginas/programacao.php
@@ -0,0 +1,6 @@
+<h2>Install Fest (das 9h às 17h)</h2>
+<h2>Área de Jogos (das 9h às 17h)</h2>
+<h2>Palestra "Desafio das Línguas" por Emilio Cid (horario a definir)</h2>
+<h2>Palestra “Sustentabilidade de Projetos de Software Livre e Padrões Abertos” por Evaldo Junior (horário a definir)</h2>
+<h2>Debate "Software Livre e a Sociedade" (horário a definir)</h2>
+
