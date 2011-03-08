a9b87306b512b70d280b9b7627aa6778a26e2d6f
InFog <junior@casoft.info>
2011-03-08 20:29:36 -0300

    Adicionando fotos.

--- paginas/fotos.php
+++ paginas/fotos.php
@@ -6,7 +6,7 @@
             <img src="<?php echo URL; ?>imagens/fotos/flisol2010-1.jpg" alt="Flisol 2010" />
         </td>
         <td>
-            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-2.jpg" alt="Flisol 2010" />
+            <img src="<?php echo URL; ?>imagens/fotos/flisol-3.jpg" alt="Flisol 2010" />
         </td>
     </tr>
     <tr>
@@ -15,6 +15,22 @@
         </td>
         <td>
             <img src="<?php echo URL; ?>imagens/fotos/flisol2010-4.jpg" alt="Flisol 2010" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-5.jpg" alt="Flisol 2010" />
+        </td>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol2010-6.jpg" alt="Flisol 2010" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol-1.jpg" alt="Flisol 2010" />
+        </td>
+        <td>
+            <img src="<?php echo URL; ?>imagens/fotos/flisol-2.jpg" alt="Flisol 2010" />
         </td>
     </tr>
 </table>
