cdab9bd51230f648bb0dd0f2cdd9a46c72b36300
InFog <junior@casoft.info>
2011-03-07 23:30:14 -0300

    Adicionando informações sobre o GCCSD.

--- paginas/organizadores.php
+++ paginas/organizadores.php
@@ -7,12 +7,14 @@
 
 <p>E foi durante 2007 que o grupo foi batizado de <strong>GCC Santos Dumont</strong>. Antes disso algumas outras opções de nome foram consideradas, já que nós não queriamos seguir o velho padrão GUSLXX (Grupo de Usuários de Software Livre de “Algum Lugar”). Um dos nomes sugeridos foi Marruá, que na cultura nordestina é usado para definir os cavalos selvagens que nunca foram domados, por isso não livres. É um ótimo nome, com um significado muito legal para o grupo, mas a opinião dos membros foi de que le não soava bem.</p>
 
-<p>Já no ano de 2008 para a realização do segundo Flisol contamos com a participação de mais pessoas, como é o caso do Sprity e do <a href="http://blog.thiagofazzio.com.br" title="Blog do Guariní">Guariní</a>, que conhecemos durante uma palestra de software livre realizada pelo Kretcheu na Unimonte.</p>
+<p>Já no ano de 2008 para a realização do segundo Flisol contamos com a participação de mais pessoas, como é o caso do Sprity e do <a href="http://blog.thiagofazzio.com.br" title="Blog do Guariní">Guariní</a>, que se juntou ao grupo após uma palestra de software livre realizada pelo Kretcheu na Unimonte.</p>
 
-<p>Durante o ano de 2008, o grupo teve a participação de apenas três membros, Guariní, InFog e Kretcheu. Tudo bem que lá de vez em quando aparecia algum rosto novo, mas acho que isso aconteceu uma ou duas vezes.</p>
+<p>Durante o ano de 2008, o grupo teve a participação de apenas três membros, Guariní, InFog e Kretcheu. Tudo bem que lá de vez em quando aparecia algum rosto novo, mas isso aconteceu uma ou duas vezes.</p>
 
-<p>No Flisol de 2009 novos membros se juntaram ao grupo, um deles é o Phoenix, e agora o compartilhamento de informações e a disseminação do software livre está ganhando força na região de Santos!</p>
+<p>No Flisol de 2009 novos membros se juntaram ao grupo, um deles é o <a href="http://mudandoparaolinux.blogspot.com" title="Mudando para o Linux">Phoenix</a>.</p>
+
+<p>Durante 2010 o grupo cresceu mais, dentre os novos membros está o <a href="http://twitter.com/DiegoSouza9" title="Twitter do Apu">Apu</a>, que contribuiu com as ideias para os nossos dojos. Hoje temos mais de 70 acessociados em nossa <a href="http://lista.gccsd.com.br/listinfo.cgi/gccsd-gccsd.com.br" title="Lista do GCCSD">lista de discussão</a>, que anda bem movimentada.</p>
 
 <h2>Sobre o nome...</h2>
 
-<p>O nome GCC faz alusão ao famoso <strong>Gnu C Compiler</strong>, o compilador do projeto Gnu, e a homenagem à Santos Dumont foi feita por que ele não patenteava suas invenções, acreditando que o progresso deve acontecer à toda a humanidade e não apenas à algumas pessoas.</p>
+<p>O nome GCC faz alusão ao famoso <a href="http://gcc.gnu.org/" title="GCC">Gnu C Compiler</a>, o compilador de C do projeto Gnu, e a homenagem à Santos Dumont foi feita por que ele não patenteava suas invenções, acreditando que o progresso deve acontecer à toda a humanidade e não apenas à algumas pessoas.</p>
