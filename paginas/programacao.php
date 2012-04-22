6a2ab47ee0cc1d252aa47916565b45dc84424fac
InFog <junior@casoft.info>
2012-04-22 09:37:00 -0300

    Alterando a programação.

--- paginas/programacao.php
+++ paginas/programacao.php
@@ -1,12 +1,1 @@
-<h1>Programação</h1>
-<h2>Dias 26 e 27/05 - Palestras e debates</h2>
-<p>Das 19h30 às 22h00, no auditório da Etec, haverá diversas palestras e debates sobre Cultura Digital e Software Livre.</p>
-<h2>Dia 28/05 - Install Fest (9h às 17h)</h2>
-<p>Durante todo o dia acontecerão instalações de softwares livres nos computadores dos participantes interessados.</p>
-<p>A equipe organizadora do Flisol Santos conta com técnicos especializados em Software Livre. Esses profissionais podem ajudar os participantes a:</p>
-<ul>
-    <li>Instalar Softwares Livres em seus computares. Sejam distribuições GNU/Linux ou softwares livres para a plataforma Windows(r).</li>
-    <li>Resolver problemas pós instalação de distribuições GNU/Linux.</li>
-    <li>Tirar dúvidas em geral sobre o uso de Softwares Livres.</li>
-</ul>
-<h2>Programação de palestras em breve.</h2>
+<h1>Programação de palestras em breve.</h1>
