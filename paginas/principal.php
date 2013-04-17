3a87299ca0f9edbe5a901c0624436cd01f6f762a
Diego Souza <diego.souza@akatus.com>
2013-04-17 08:13:43 -0300

    Vídeo de divulgação c/ Kretcheu na página inicial

--- paginas/principal.php
+++ paginas/principal.php
@@ -1,9 +1,11 @@
 <h2>Venha até o SESC Santos e participe do FLISOL 2013!</h2>
 
+<iframe width="560" height="315" src="http://www.youtube.com/embed/c-U-U6FMjSg" frameborder="0" allowfullscreen></iframe>
+
+<br><br>
+
 <p>O <strong>FLISOL</strong> é o maior evento de difusão de Software Livre na América Latina. Vem sendo realizado desde 2005 e tem como objetivo principal, além de promover o uso do Software Livre, esclarecer sua filosofia, avanços, viabilidade técnica, social e econômica. O evento é destinado a estudantes, empresários e o público em geral.
 </p>
-<p>Durante o <strong>FLISOL</strong> serão discutidos o uso do Software Livre como forma de emancipação dos usuários na rede mundial de computadores, além da necessidade de se colocar os usuários em posição de equilíbrio com os desenvolvedores e empresas produtoras de software. “Esse equilíbrio interfere nos sistemas de desenvolvimento e de acesso aos conhecimentos, tornando todos partícipes do processo”, afirma Paulo Kretcheu, professor universitário e consultor de segurança e redes.</p>
-
-<p>Segundo <strong><a href="http://kretcheu.com.br/">Kretcheu</a></strong>, o usuário de softwares livres, ainda que sem perceber, fortalece o modelo de desenvolvimento colaborativo e acaba por se beneficiar mesmo que não envolvido diretamente. O usuário de software proprietário, em boa parte usa de modo ilegal, cometendo um crime e ficando cada vez mais dependente do modelo que lhe desfavorece.</p>
 
 <p>O evento se propõe a debater a produção de conteúdo de forma colaborativa na Internet, quais são os limites da liberdade de criação e inovação, leis de direitos autorais, as diferenças entre os softwares livres e proprietários, entre outros assuntos em pauta.</p>
+
