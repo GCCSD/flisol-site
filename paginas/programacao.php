69c550568638db9e73e52d3fee863134bdac45a3
Evaldo Junior <evaldojuniorbento@gmail.com>
2014-03-14 10:25:06 -0300

    Removendo programação antiga

--- paginas/programacao.php
+++ paginas/programacao.php
@@ -36,22 +36,3 @@
 		<td>Carlos Ribeiro</td>
 	</tr>
 </table>
-<!--
-<h1>Programação</h1>
-
-<p>A definir</p>
-
-
-
-<p>Durante todo o evento teremos um espaço de jogos, demonstração de ambientes gráficos, instalação de distribuições GNU/Linux e outros softwares livres.</p>
-
-<ul>
-    <li>11h - <strong>Palestra "Software Livre"</strong> - Kretcheu</li>
-    <li>13h - <strong>Debate "Dispositivos Móveis"</strong></li>
-    <li>14h - <strong>Palestra "Ambiente livre para desenvolvimento web"</strong> - InFog</li>
-    <li>15h - <strong>Raspberry PI e Raspbian "Wheezy"</strong> - Thiago Ferauche</li>
-    <li>16h - <strong>Palestra "IPv6"</strong> - Kretcheu</li>
-    <li>17h - <strong>Debate - "A Mozilla e o novo desafio web"</strong> - Coragem</li>
-</ul>
-
--->
