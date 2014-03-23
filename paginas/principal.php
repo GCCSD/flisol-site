c654d420ff3c767203170a67160ed3743be18da8
Evaldo Junior <evaldojuniorbento@gmail.com>
2014-03-23 12:18:30 -0300

    Topo e retirei o vídeo.

--- paginas/principal.php
+++ paginas/principal.php
@@ -1,9 +1,5 @@
 <h2>Venha até o SESC Santos e participe do FLISOL 2014!</h2>
 
-<iframe width="560" height="315" src="http://www.youtube.com/embed/c-U-U6FMjSg" frameborder="0" allowfullscreen></iframe>
-
-<br><br>
-
 <p>O <strong>FLISOL</strong> é o maior evento de difusão de Software Livre na América Latina. Vem sendo realizado desde 2005 e tem como objetivo principal, além de promover o uso do Software Livre, esclarecer sua filosofia, avanços, viabilidade técnica, social e econômica. O evento é destinado a estudantes, empresários e o público em geral.
 </p>
 
