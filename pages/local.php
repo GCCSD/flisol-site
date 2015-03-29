166a39e611d2ad5ee5b4e00ecbf6a22ee2fcbf6e
Evaldo Junior <evaldojuniorbento@gmail.com>
2015-03-29 11:59:00 +0200

    Adicionado mapa na página da localizacao.

--- pages/local.php
+++ pages/local.php
@@ -1,6 +1,7 @@
 <h2>Onde?</h2>
 
 <p>
-    Ainda não definimos o local para o Flisol Santos 2015.
-    Em breve teremos mais novidades.
+    O Flisol Santos 2015 acontecerá no LabLivre do Sesc Santos, na Rua Conselheiro Ribas, 136, Aparecida, Santos/SP.
 </p>
+
+<iframe class="maps" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-46.31373256444931%2C-23.976881235044942%2C-46.31019204854965%2C-23.97466327827949&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/#map=19/-23.97577/-46.31196">Ver mapa no OpenStreetMap</a></small>
