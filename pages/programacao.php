a337926fbd6a1166afdd6b2056bbf9200388c181
Diego Souza <diegosouza.br@gmail.com>
2015-04-22 13:20:22 -0300

    Colocando o horário das palestras

--- pages/programacao.php
+++ pages/programacao.php
@@ -1,17 +1,17 @@
 <h2>Programação Flisol 2015</h2>
 
-<h3>Introducao ao Software Livre</h3>
+<h3>14h - Introducao ao Software Livre</h3>
 <p class="autor"><img src="<?php echo URL; ?>assets/images/speakers/kretcheu.jpg" />Paulo Kretcheu</p>
 <p>Aprenda o que é software livre, desvende alguns mitos que cercam o software livre e descubra um novo mundo de possibilidades.</p>
 
-<h3>Como Contribuir para um Projeto de Software Livre</h3>
-<p class="autor"><img src="<?php echo URL; ?>assets/images/speakers/taiane.jpg" />Taiane Ramos</p>
-<p>Muitas pessoas gostariam de contribuir para projetos de software livre, mas não tem nem ideia de por onde começar. Nesta palestra, pretendo apresentar algumas formas de contribuição (tradução, documentação, teste, arte, código) e dar uma visão geral de primeiros passos a seguir para conseguir contribuir para o projeto.</p>
-
-<h3>Por que Git e GitHub?</h3>
+<h3>15h - Por que Git e GitHub?</h3>
 <p class="autor"><img src="<?php echo URL; ?>assets/images/speakers/diego.png" />Diego Souza</p>
 <p>Sistemas de controle de versão: introdução, por que versionar e breve comparativo com foco no Git. GitHub: visão geral, recursos, importância no mundo do software livre.</p>
 
-<h3>LibreOffice: navegando pelo projeto</h3>
+<h3>16h - LibreOffice: navegando pelo projeto</h3>
 <p class="autor"><img src="<?php echo URL; ?>assets/images/speakers/douglas.jpg" />Douglas Vigliazzi</p>
 <p>Apresentar LibreOffice mantido pela TDF e todas as ramificações do projeto que tornam o LibreOffice a melhor suíte de escritório, livre e de código aberto, do mundo.</p>
+
+<h3>17h - Como Contribuir para um Projeto de Software Livre</h3>
+<p class="autor"><img src="<?php echo URL; ?>assets/images/speakers/taiane.jpg" />Taiane Ramos</p>
+<p>Muitas pessoas gostariam de contribuir para projetos de software livre, mas não tem nem ideia de por onde começar. Nesta palestra, pretendo apresentar algumas formas de contribuição (tradução, documentação, teste, arte, código) e dar uma visão geral de primeiros passos a seguir para conseguir contribuir para o projeto.</p>
